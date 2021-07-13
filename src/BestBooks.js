b6cf18e9b8bd8aa1a7e9dde3c65b9780c224bd08
Ghaidaamoh <ghaidaa97mohammad@gmail.com>
2021-07-13 18:04:25 +0300

    class13frontend

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -2,8 +2,8 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import './BestBooks.css';
+import { Button, Card, Modal, Form } from 'react-bootstrap/';
 import { withAuth0 } from '@auth0/auth0-react';
-import Card from 'react-bootstrap/Card'
 import axios from 'axios'
 class MyFavoriteBooks extends React.Component {
   constructor(props) {
@@ -34,6 +34,47 @@ class MyFavoriteBooks extends React.Component {
     })
 
   }
+  //////////
+  handelClick = () => {
+    this.setState({
+      showBooks: true,
+    });
+  }
+
+  handleClose = () => {
+    this.setState({
+      showBooks: false,
+    })
+  }
+
+  addBook = async (event) => {
+    event.preventDefault();
+    //console.log(event.target.bookName.value);
+    const server = process.env.REACT_APP_PORT;
+    const newBook = {
+      name: event.target.bookName.value,
+      description: event.target.description.value,
+      status: event.target.status.value,
+      userEmail: this.state.userEmail
+
+    }
+    const addBookUrl = await axios.post(`${server}/addBook`, newBook);
+    this.setState({
+      booksArr: addBookUrl.data,
+    })
+  }
+
+  deleteBook = async (idx) => {
+    const server = process.env.REACT_APP_PORT;
+    const deletPara = {
+      email: this.state.userEmail,
+      index: idx
+    }
+    const deletURL = await axios.delete(`${server}/deleteBook`, { params: deletPara });
+    this.setState({
+      booksArr: deletURL.data,
+    });
+  }
 
   render() {
     return (
@@ -43,13 +84,15 @@ class MyFavoriteBooks extends React.Component {
           This is a collection of my favorite books
         </p>
         <div className="bookcont">
+          <Button variant="warning" onClick={this.handelClick} >Add Book</Button>
+
           {
             this.state.showBooks &&
-            this.state.booksArr.map(item => {
+            this.state.booksArr.map((item, inx) => {
 
 
               return (
-                <Card >
+                <Card  key={inx.toString()}>
                   <Card.Body>
                     <Card.Title>{item.name}</Card.Title>
                     <Card.Img src={item.img} alt={item.name} />
@@ -59,6 +102,7 @@ class MyFavoriteBooks extends React.Component {
                     <Card.Text>
                       {item.status}
                     </Card.Text>
+                    <Button variant="warning" onClick={()=>this.deleteBook(inx)}>Delete</Button>
                   </Card.Body>
                 </Card>
               )
@@ -66,9 +110,42 @@ class MyFavoriteBooks extends React.Component {
             })
 
           }
+          <>
+            <Modal show={this.state.showBooks} onHide={this.handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title>Add Book</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <Form >
+                  <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Book name</Form.Label>
+                    <Form.Control  onSubmit={this.addBook} type="text" placeholder="book name" name='bookName' />
+
+                  </Form.Group>
+
+                  <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Book description</Form.Label>
+                    <Form.Control type="text" placeholder="description" name='description' />
+                  </Form.Group>
+                  <Form.Group controlId="status">
+                    <Form.Label>Book status</Form.Label>
+                    <Form.Control type="text" placeholder="status" name='status' />
+                  </Form.Group>
+                  <Button variant="warning" type="submit">
+                    Submit
+                  </Button>
+                </Form>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={this.handleClose}>
+                  Close
+                </Button>
+
+              </Modal.Footer>
+            </Modal>
+          </>
+         
         </div>
-
-
 
       </Jumbotron>
     )
